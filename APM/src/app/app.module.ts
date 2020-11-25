eb49b69c247acb9410a0219060cd319b3503ab03
Brook Hutchinson <Brookhutchinson@outlook.com>
2020-11-25 12:14:34 -0600

    Setup and configure Angular Router When user clicks "Home" link inside AppComponent HTML Template then navigate to WelcomeComponent HTML Template When user clicks "Product List" link inside AppComponent HTML Template then navigate to ProductListComponent HTML Template Register RouterModule with AppModule Set [routerLink] directive of "Home" link to '/welcome' route inside AppComponent HTML Template Set [routerLink] directive of "Product List" link to '/products' route inside AppComponent HTML Template Define <div class="container"> under navbar that will contain navigated content Define <router-outlet> inside <div class="container"> Create AppRoutingModule Define route with path 'welcome that navigates to WelcomeComponent Define default route path with path '' that redirects to WelcomeComponent Define wildcard route with path '**' that redirects to WelcomeComponent Register AppRoutingModule with AppModule

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -2,10 +2,14 @@
 import { NgModule }         from '@angular/core';
 import { BrowserModule }    from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule }     from '@angular/router';
 
 // feature modules
 import { HomeModule }       from './modules/home/home.module';
 import { ProductModule }    from './modules/product/product.module';
+
+// app routing module
+import { AppRoutingModule } from './app-routing.module';
 
 // components
 import { AppComponent }     from './app.component';
@@ -14,9 +18,11 @@ import { AppComponent }     from './app.component';
   // modules
   imports: [
     // angular modules
-    BrowserModule, HttpClientModule,
+    BrowserModule, HttpClientModule, RouterModule,
     // feature modules
-    HomeModule, ProductModule
+    HomeModule, ProductModule,
+    // app routing module
+    AppRoutingModule
   ],
   // components
   declarations: [ AppComponent ],
