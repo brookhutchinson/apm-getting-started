7be73fa3a2742937bb3afc2739ccc1658f95d07f
Brook Hutchinson <Brookhutchinson@outlook.com>
2020-11-25 11:09:25 -0600

    Register HttpClientModule with AppModule

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -1,19 +1,20 @@
 // angular modules
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule }         from '@angular/core';
+import { BrowserModule }    from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 // feature modules
-import { HomeModule }    from './modules/home/home.module';
-import { ProductModule } from './modules/product/product.module';
+import { HomeModule }       from './modules/home/home.module';
+import { ProductModule }    from './modules/product/product.module';
 
 // components
-import { AppComponent }  from './app.component';
+import { AppComponent }     from './app.component';
 
 @NgModule({
   // modules
   imports: [
     // angular modules
-    BrowserModule,
+    BrowserModule, HttpClientModule,
     // feature modules
     HomeModule, ProductModule
   ],
