9f247b521140efaa1afde7bc20c85e2b674ebcfd
Brook Hutchinson <Brookhutchinson@outlook.com>
2020-11-25 13:59:47 -0600

    Create ProductListComponent Register ProductListComponent with ProductModule Export ProductListComponent from ProductModule Define ProductListComponent HTML Template

--- APM/src/app/modules/product/product.module.ts
+++ APM/src/app/modules/product/product.module.ts
@@ -4,19 +4,23 @@ import { NgModule }               from '@angular/core';
 // shared modules
 import { SharedModule }           from './../shared/shared.module';
 
+// components
+import { ProductListComponent }   from './components/product-list/product-list.component';
+
 @NgModule({
   // modules
   imports: [
     // shared modules
     SharedModule
   ],
-  // components
   declarations: [
-
+    // components
+    ProductListComponent
   ],
   // exports
   exports: [
-
+    // components
+    ProductListComponent
   ]
 })
 export class ProductModule {}
