3a3c3334af3c09481f768bece4d656a121237a0d
Brook Hutchinson <Brookhutchinson@outlook.com>
2020-12-08 11:25:56 -0600

    Change text color of table column headers to blue

--- APM/src/app/modules/product/components/product-list/product-list.component.ts
+++ APM/src/app/modules/product/components/product-list/product-list.component.ts
@@ -7,7 +7,7 @@ import { Product }           from './../../../../interfaces/product';
 @Component({
   selector: 'product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent implements OnInit {
   title: string = 'Product List';
