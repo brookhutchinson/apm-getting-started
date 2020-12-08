0069157ef25badba9b81a915f32e8b895ef0fc22
Brook Hutchinson <Brookhutchinson@outlook.com>
2020-12-08 11:45:02 -0600

    Create pipe named ConvertToSpacesPipe inside SharedModule ConvertToSpacesPipe is used to change dash characters to space characters Import pipe named ConvertToSpaces into SharedModule Export pipe named ConvertToSpaces from SharedModule Apply ConvertToSpacesPipe to productCode for each product inside ProductListComponent

---
+++ APM/src/app/modules/shared/pipes/convert-to-spaces.pipe.spec.ts
@@ -0,0 +1,9 @@
+// pipes
+import { ConvertToSpacesPipe } from './convert-to-spaces.pipe';
+
+describe('ConvertToSpacesPipe', () => {
+  it('should create ConvertToSpacesPipe', () => {
+    const pipe = new ConvertToSpacesPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
--- APM/src/app/modules/shared/shared.module.ts
+++ APM/src/app/modules/shared/shared.module.ts
@@ -1,7 +1,10 @@
-// modules
-import { NgModule }     from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule }  from '@angular/forms';
+// angular modules
+import { NgModule }            from '@angular/core';
+import { CommonModule }        from '@angular/common';
+import { FormsModule }         from '@angular/forms';
+
+// pipes
+import { ConvertToSpacesPipe } from './pipes/convert-to-spaces.pipe';
 
 @NgModule({
   // modules
@@ -12,13 +15,16 @@ import { FormsModule }  from '@angular/forms';
   ],
   // components
   declarations: [
-
+    // pipes
+    ConvertToSpacesPipe
   ],
   // exports
   exports: [
     // angular modules
     CommonModule,
-    FormsModule
+    FormsModule,
+    // pipes
+    ConvertToSpacesPipe
   ]
 })
 export class SharedModule {}
