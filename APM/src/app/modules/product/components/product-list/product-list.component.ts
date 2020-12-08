13c2cc12d40b6135a44c991f43be213bb0f580c8
Brook Hutchinson <Brookhutchinson@outlook.com>
2020-12-08 12:22:27 -0600

    When user types value inside <input type="text"> then show only those products that match user entered filter text Create property named filteredProducts of type Product[] Use constructor() method to initialize filteredProducts to original products Create backing property named _filterText of type string Create getter() function Create setter() function Create performFilter() function that receives the filter text and then returns the filtered products Bind each row inside HTML Template to filteredProducts property

--- APM/src/app/modules/product/components/product-list/product-list.component.ts
+++ APM/src/app/modules/product/components/product-list/product-list.component.ts
@@ -16,8 +16,23 @@ export class ProductListComponent implements OnInit {
   imageMargin: number = 2;
   showImage: boolean = false;
 
-  listFilter: string = 'cart';
+  // backing property
+  private _filterText: string;
+  get filterText(): string {
+    return this._filterText;
+  }
+  set filterText(value: string) {
+    this._filterText = value;
 
+    // if there is a value for filterText then apply filter and display filtered products
+    // if there is no value for filterText then display all products
+    this.filteredProducts = this.filterText ? this.performFilter(this.filterText) : this.products;
+  }
+
+  // filtered products
+  filteredProducts: Product[];
+
+  // original products
   products: Product[] = [
     {
       "productId": 1,
@@ -71,9 +86,23 @@ export class ProductListComponent implements OnInit {
     }
   ];
 
-  constructor() {}
+  constructor() {
+    // set filtered products to original products
+    this.filteredProducts = this.products;
+  }
 
   ngOnInit() {}
+
+  performFilter(filterText: string): Product[] {
+    // convert filterText to lowercase
+    filterText = filterText.toLocaleLowerCase();
+
+    // goal: create create array of filtered products
+    // 1) for each product in the products array check whether the filterText exists inside the productName
+    // 2) if filterText exists inside productName then add product to array of filtered products
+    // 3) return array of filtered products
+    return this.products.filter((product: Product) => product.productName.toLocaleLowerCase().indexOf(filterText) !== -1);
+  }
 
   toggleImage() {
     this.showImage = !this.showImage;
