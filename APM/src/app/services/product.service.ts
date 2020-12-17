ea1421a311e578ef73969335f61d01c16ed37d25
Brook Hutchinson <Brookhutchinson@outlook.com>
2020-12-17 09:54:30 -0600

    Use Http with Observables to retrieve data from JSON file Register HttpClientModule inside AppModule Import HttpClient into ProductService Inject property http: HttpClient into constructor() method Import Observable Import throwError() operator Import catchError() operator Import map() operator Import tap() operator

--- APM/src/app/services/product.service.ts
+++ APM/src/app/services/product.service.ts
@@ -1,14 +1,22 @@
 // services
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 // interface
-import { Product }    from '../interfaces/product';
+import { Product }    from './../interfaces/product';
+
+// rxjs
+import { Observable } from 'rxjs';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { map }        from 'rxjs/operators';
+import { tap }        from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   getProducts(): Product[] {
     return [
