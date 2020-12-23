c2e512752fc9d5a9dc3cdfc96878b4ed1265f8b1
Brook Hutchinson <Brookhutchinson@outlook.com>
2020-12-23 10:39:51 -0600

    Render product.productId of selected product inside card header of ProductDetailComponent HTML Template Define a property named product of type Product Set product property to object using hardcoded properties Set product.productId to id from selected product route

--- APM/src/app/modules/product/components/product-detail/product-detail.component.ts
+++ APM/src/app/modules/product/components/product-detail/product-detail.component.ts
@@ -20,5 +20,16 @@ export class ProductDetailComponent implements OnInit {
 
   ngOnInit() {
     let id = +this.route.snapshot.paramMap.get('id');
+
+    this.product = {
+      'productId': id,
+      'productName': 'Leaf Rake',
+      'productCode': 'GDN-0011',
+      'releaseDate': 'March 19, 2019',
+      'description': 'Leaf rake with 48-inch wooden handle',
+      'price': 19.95,
+      'starRating': 3.2,
+      'imageUrl': 'assets/images/leaf_rake.png'
+    }
   }
 }
