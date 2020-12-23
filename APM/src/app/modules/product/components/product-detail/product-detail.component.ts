850c3fa071dc3f37661e676f3c658ebe3be67697
Brook Hutchinson <Brookhutchinson@outlook.com>
2020-12-23 09:53:38 -0600

    Define ProductDetailComponent HTML Template Define a card that contains a card header inside ProductDetailComponent HTML Template Render pageTitle and product.productName inside card header of ProductDetailComponent HTML Template Use *ngIf directive to not display ProductDetailComponent HTML Template if product is null or not defined Use *ngIf directive to display ProductDetailComponent HTML Template if product is not null or is defined

--- APM/src/app/modules/product/components/product-detail/product-detail.component.ts
+++ APM/src/app/modules/product/components/product-detail/product-detail.component.ts
@@ -1,12 +1,18 @@
 // components
 import { Component, OnInit } from '@angular/core';
 
+// interfaces
+import { Product }           from './../../../../interfaces/product';
+
 @Component({
   selector: 'product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.scss']
 })
 export class ProductDetailComponent implements OnInit {
+  pageTitle: string = 'Product Detail';
+  product: Product;
+
   constructor() {}
 
   ngOnInit() {}
