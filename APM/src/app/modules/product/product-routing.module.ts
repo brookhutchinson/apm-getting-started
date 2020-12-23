931a0d262e4c120b37ca45119e126d0a94f7e201
Brook Hutchinson <Brookhutchinson@outlook.com>
2020-12-23 10:03:54 -0600

    Add ProductDetailComponent route to routes defined inside ProductRoutingModule Pass product id of selected product to ProductDetailComponent route using route parameter named id Import ProductDetailComponent into ProductRoutingModule

--- APM/src/app/modules/product/product-routing.module.ts
+++ APM/src/app/modules/product/product-routing.module.ts
@@ -4,12 +4,15 @@ import { RouterModule }           from '@angular/router';
 import { Routes }                 from '@angular/router';
 
 // components
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { ProductListComponent }   from './components/product-list/product-list.component';
 
 // define routes
 const routes: Routes = [
   // product list route
-  { path: 'products', component: ProductListComponent }
+  { path: 'products', component: ProductListComponent },
+  // product detail route
+  { path: 'products/:id', component: ProductDetailComponent }
 ];
 
 @NgModule({
